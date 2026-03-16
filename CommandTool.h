cb40d76272ddf1f27b7053c54af9e2d78890dfdb
Finley Bourke <fin.bourke11@gmail.com>
2026-03-16 00:45:15 +0000

    practice

--- CommandTool.h
+++ CommandTool.h
@@ -1,17 +1,57 @@
 #include <iostream>
+#include <unordered_map>
+#include <functional>
+#include <string>
+#include "Task.h"
+#include "TaskManager.h"
+#include <vector>
+#include "Util.h"
 
 class CommandTool {
 
-    std::unordered_map<std::string, std::function<void()>> commands = {
-        {"add", addTask},
-        {"list", listTask},
-        {"exit", exitApp}
-    };
+    private:
+        std::unordered_map<std::string, std::function<void()>> commands;
+        TaskManager tm;
+    public: 
+        CommandTool(){ // capture current object, no parameters, run this lambda
+            commands["add"] = [this]() {addTask();};
+            commands["list"] = [this]() {listTask();};
+            commands["exit"] = [this]() {exitApp();}
+        }
+        void findCommand(std::string command){
 
-    void findCommand(std::string command){
+            // find returns an iterator pointing to the pair
+            auto it = commands.find(command);
+            if (it == commands.end()){
+                std::cout<< "Command Unknown";
+                return;
+            }
 
-        auto func = commands[command]
+            it->second();
+        }
+    
+    private:
+        void addTask(){
+            std::cout<<"List Description, Priority, Hours Allocated";
+            std::string details;
+    
+            std::getline(std::cin, details);
+
+            std::vector<std::string> tokens = split(details);
+            
+            if (tokens.empty()) return;
+
+            
 
 
-    }
+            
+        }
+
+        void listTask(){
+            return;
+        }
+
+        void exitApp(){
+            return;
+        }
 }
